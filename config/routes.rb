7efe9d2febfb935e6d871ba6e7431c24004f3073
Jeffrey Orielly <Jeffreyaeorielly@gmail.com>
2019-06-23 22:42:36 +0200

    version 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
+ # root to: 'pages#index'
   resources :pages
-  #root 'home#index'
+  root 'home#index'
   #get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
