8d3450ed9670e3da7705d86f9d4675e0a44ff6b8
Jeffrey Orielly <Jeffreyaeorielly@gmail.com>
2019-06-23 22:27:18 +0200

    pages and scarfolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/index'
+  #root to: 'pages#index'
+  resources :pages
+  root to: 'home#index'
+  #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
