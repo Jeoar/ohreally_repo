0802f71be96844ca572b7b79de40909ee6a033a7
Jeffrey Orielly <Jeffreyaeorielly@gmail.com>
2019-06-26 14:46:22 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: hero'pages#index'
   resources :pages
+  root to: hero'pages#index'
   #root to: 'home#index'
   #get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
